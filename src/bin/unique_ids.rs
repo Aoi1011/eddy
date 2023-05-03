a57d3248551afb7d534a56e7fa86bf0f50e7ecf6
Aoi1011 <ht.so.ik0102@gmail.com>
2023-05-04 08:44:23 +0900

    feat: add solution to unique-ids challenge

--- src/bin/unique_ids.rs
+++ src/bin/unique_ids.rs
@@ -1,50 +1,40 @@
 use std::io::{StdoutLock, Write};
 
-use anyhow::{bail, Context};
+use anyhow::Context;
 use eddy::*;
 use serde::{Deserialize, Serialize};
-use ulid::Ulid;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
 enum Payload {
-    Generate    , 
+    Generate,
     GenerateOk {
         #[serde(rename = "id")]
         guid: String,
     },
-    Init {
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    InitOk,
 }
 
 struct UniqueNode {
+    node: String,
     id: usize,
 }
 
-impl Node<Payload> for UniqueNode {
+impl Node<(), Payload> for UniqueNode {
+    fn from_init(_state: (), init: Init) -> anyhow::Result<Self>
+    where
+        Self: Sized,
+    {
+        Ok(UniqueNode {
+            node: init.node_id,
+            id: 1,
+        })
+    }
+
     fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
         match input.body.payload {
-            Payload::Init { .. } => {
-                let reply = Message {
-                    src: input.dst,
-                    dst: input.src,
-                    body: Body {
-                        id: Some(self.id),
-                        in_reply_to: input.body.id,
-                        payload: Payload::InitOk,
-                    },
-                };
-                serde_json::to_writer(&mut *output, &reply)
-                    .context("serialize response to init")?;
-                output.write_all(b"\n").context("write traling newline")?;
-                self.id += 1;
-            }
-            Payload::Generate  => {
-                let guid = Ulid::new().to_string();
+            Payload::Generate => {
+                let guid = format!("{}-{}", self.node, self.id);
                 let reply = Message {
                     src: input.dst,
                     dst: input.src,
@@ -60,7 +50,6 @@ impl Node<Payload> for UniqueNode {
 
                 self.id += 1;
             }
-            Payload::InitOk { .. } => bail!("received init_ok message"),
             Payload::GenerateOk { .. } => {}
         }
         Ok(())
@@ -68,5 +57,5 @@ impl Node<Payload> for UniqueNode {
 }
 
 fn main() -> anyhow::Result<()> {
-    main_loop(UniqueNode { id: 0 })
+    main_loop::<_, UniqueNode, _>(())
 }

6ff87dc153f0b6df9dde6c99dda6f9af35d1c919
Aoi1011 <ht.so.ik0102@gmail.com>
2023-05-04 08:43:51 +0900

    fix: change logic echo

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,6 +1,6 @@
 use std::io::{StdoutLock, Write};
 
-use anyhow::{bail, Context};
+use anyhow::Context;
 use eddy::*;
 use serde::{Deserialize, Serialize};
 
@@ -8,41 +8,23 @@ use serde::{Deserialize, Serialize};
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
 enum Payload {
-    Echo {
-        echo: String,
-    },
-    EchoOk {
-        echo: String,
-    },
-    Init {
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    InitOk,
+    Echo { echo: String },
+    EchoOk { echo: String },
 }
 
 struct EchoNode {
     id: usize,
 }
 
-impl Node<Payload> for EchoNode {
+impl Node<(), Payload> for EchoNode {
+    fn from_init(_state: (), _init: Init) -> anyhow::Result<Self>
+    where
+        Self: Sized,
+    {
+        Ok(EchoNode { id: 1 })
+    }
     fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
         match input.body.payload {
-            Payload::Init { .. } => {
-                let reply = Message {
-                    src: input.dst,
-                    dst: input.src,
-                    body: Body {
-                        id: Some(self.id),
-                        in_reply_to: input.body.id,
-                        payload: Payload::InitOk,
-                    },
-                };
-                serde_json::to_writer(&mut *output, &reply)
-                    .context("serialize response to init")?;
-                output.write_all(b"\n").context("write traling newline")?;
-                self.id += 1;
-            }
             Payload::Echo { echo } => {
                 let reply = Message {
                     src: input.dst,
@@ -60,12 +42,11 @@ impl Node<Payload> for EchoNode {
                 self.id += 1;
             }
             Payload::EchoOk { .. } => {}
-            Payload::InitOk { .. } => bail!("received init_ok message"),
         }
         Ok(())
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    main_loop(EchoNode { id: 0 })
+    main_loop::<_, EchoNode, _>(())
 }
